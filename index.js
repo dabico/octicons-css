1df660f32c9e5cf6c46c73c346ecd0580cc8ffa0
dabico <dabico@usi.ch>
2023-06-19 10:02:32 +0200

    All octicons are now in a single stylesheet

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const fs = require("node:fs/promises");
 const axios = require("axios");
 const retry = require("axios-retry");
 const ghls = require("list-github-dir-content");
-const {glob} = require("glob");
-const {Presets, SingleBar, MultiBar} = require("cli-progress");
+const {Presets, SingleBar} = require("cli-progress");
 const {FontAssetType, OtherAssetType, generateFonts} = require("fantasticon");
 
 const defaultFontConfig = {
@@ -57,15 +56,12 @@ async function downloadIcons() {
         }),
     ]);
 
-    const regex = /-(12|16|24)\.svg$/;
-    const svgs = files.filter(file => regex.test(file));
-
     console.info("Downloading icon SVGs...");
     const options = {hideCursor: true, format: "{bar} {percentage}% | ETA: {eta}s | {value}/{total}"};
     const progressbar = new SingleBar(options, Presets.shades_classic);
-    progressbar.start(svgs.length, 0);
+    progressbar.start(files.length, 0);
     await Promise.all(
-        svgs.map(async svg => {
+        files.map(async svg => {
             const base = "https://raw.githubusercontent.com/primer/octicons/main";
             const config = {responseType: "arraybuffer", timeout: 10 * 1000};
             const response = await axios.get(`${base}/${svg}`, config);
@@ -77,62 +73,26 @@ async function downloadIcons() {
     progressbar.stop();
 }
 
-async function organizeIcons(...sizes) {
-    await Promise.all(
-        sizes.map(async size => fs.mkdir(`./icons/${size}`, {recursive: true}))
-    );
-    console.info("Grouping icons into size categories...");
-    const options = {hideCursor: true, format: "{bar} {size}px icons | {value}/{total}"};
-    const multibar = new MultiBar(options, Presets.shades_classic);
-    const buckets = await Promise.all(
-        sizes.map(async size => {
-            const files = await glob(`icons/*-${size}.svg`);
-            const count = files.length;
-            const bar = multibar.create(count, 0, {size: size});
-            return [files, size, bar];
-        })
-    );
-    await Promise.all(
-        buckets.map(async ([files, size, bar]) => {
-            await Promise.all(
-                files.map(async file => {
-                    const [_, name] = file.split("/");
-                    await fs.rename(file, `./icons/${size}/${name}`);
-                    bar.increment();
-                })
-            );
-        })
-    );
-    multibar.stop();
-}
-
-async function generateCSS(...sizes) {
-    await Promise.all(
-        sizes.map(async size => fs.mkdir(`./octicons/octicons-${size}/fonts`, {recursive: true}))
-    );
-    console.info("Generating CSS stylesheets...");
-    const options = {hideCursor: true, format: "{bar} {percentage}% | ETA: {eta}s | {value}/{total}"};
+async function generateCSS() {
+    await fs.mkdir("./octicons/fonts", {recursive: true});
+    console.info("Generating CSS stylesheet...");
+    const options = {hideCursor: true, format: "{bar} {percentage}% | ETA: {eta}s"};
     const progressbar = new SingleBar(options, Presets.shades_classic);
-    progressbar.start(sizes.length, 0);
-    await Promise.all(
-        sizes.map(async size => {
-            const inRoot = `./icons/${size}`;
-            const outRoot = `./octicons/octicons-${size}`;
-            const fontRoot = `${outRoot}/fonts`;
-            const options = {
-                inputDir: inRoot,
-                outputDir: outRoot,
-                pathOptions: {
-                    woff: `${fontRoot}/octicons.woff`,
-                    woff2: `${fontRoot}/octicons.woff2`,
-                    eot: `${fontRoot}/octicons.eot`,
-                },
-                ...defaultFontConfig
-            };
-            await generateFonts(options);
-            progressbar.increment();
-        })
-    );
+    progressbar.start(1, 0);
+    const inRoot = "./icons";
+    const outRoot = "./octicons";
+    const fontRoot = `${outRoot}/fonts`;
+    await generateFonts({
+        inputDir: inRoot,
+        outputDir: outRoot,
+        pathOptions: {
+            woff: `${fontRoot}/octicons.woff`,
+            woff2: `${fontRoot}/octicons.woff2`,
+            eot: `${fontRoot}/octicons.eot`,
+        },
+        ...defaultFontConfig
+    });
+    progressbar.increment();
     progressbar.stop();
 }
 
@@ -140,6 +100,5 @@ Promise.resolve()
     .then(() => setup())
     .then(() => cleanup("octicons", "icons"))
     .then(() => downloadIcons())
-    .then(() => organizeIcons(12, 16, 24))
-    .then(() => generateCSS(12, 16, 24))
+    .then(() => generateCSS())
     .then(() => cleanup("icons"));
